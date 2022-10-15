55430e7db03152875389efebe9d4698933aa8cbb
maddy <mbmadhurai@gmail.com>
2022-10-15 21:18:36 +0530

    Added NavLink

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 const Navbar = () => {
     return ( 
         <nav className="navbar navbar-expand-lg bg-light">
             <ul className="navbar-nav">
                 <li className="nav-item">
-                    <Link className="nav-link" to="/">Home</Link>
+                    <NavLink className="nav-link" to="/">Home</NavLink>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/about">About</Link>
+                    <NavLink className="nav-link" to="/about">About</NavLink>
                 </li>
             </ul>
         </nav>
