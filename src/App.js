c578955f66567f426efa6a94d0a1ce42a480de20
maddy <mbmadhurai@gmail.com>
2022-10-15 21:59:24 +0530

    No match route

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import { About } from "./components/about";
 import { Home } from "./components/home";
 import Navbar from "./components/navbar";
+import NoMatch from "./components/noMatch";
+import OrderSummary from "./components/orderSummary";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="about" element={<About />}></Route>
         <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="order-summary" element={<OrderSummary />}></Route>
+        <Route path="*" element={<NoMatch />}></Route>
       </Routes>
     </div>
   );
